d04d2faff1b55b3853433b1fbdb5236076fe6d1c
Viktar Zanimonets <vzanimonets@gmail.com>
2023-01-12 22:19:02 +0300

    feat: implementation of the endpoint api/users:GET api/users/{userId}

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,11 @@ export const server = http.createServer(async (req: any, res: any) => {
       return;
     }
 
+    if (req.url.match(/\/api\/users\/([a-z0-9-\w]+)/) && req.method === 'GET') {
+      await getUserById(req, res);
+      return;
+    }
+
     res.writeHead(404, { 'Content-Type': 'application/json' });
     res.end('Route not found');
   },
