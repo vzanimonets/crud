56da38b240375d384bafed6f4d09a94d58e3a727
Viktar Zanimonets <vzanimonets@gmail.com>
2023-01-12 22:13:23 +0300

    feat: implementation of the endpoint api/users:GET api/users

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import http from 'http';
+import * as dotenv from 'dotenv';
+import { addUser, deleteUserById, getUserById, getUsersList, updateUserById } from './user.controller';
+
+dotenv.config();
+
+if (!process.env.PORT) {
+  console.log('Error to get port!');
+}
+
+const PORT: number = parseInt(process.env.PORT as string, 10);
+
+export const server = http.createServer(async (req: any, res: any) => {
+
+    if (req.url === '/api/users' && req.method === 'GET') {
+      await getUsersList(req, res);
+      return;
+    }
+
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.end('Route not found');
+  },
+);
+
+server.listen(PORT, () => {
+  console.log(`server started on port: ${PORT}`);
+});
